3300793cfd7dbcc99cf66b8572e5a5bc5a5dc191
tsumu-github <business000t2r@gmail.com>
2024-03-06 19:24:41 +0900

    13:job_profileにshare_gazoカラムを追加

--- isekaijob_work/db/schema.rb
+++ isekaijob_work/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_23_063851) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_06_100747) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -43,6 +43,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_23_063851) do
     t.text "compatibility"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "share_gazo"
     t.index ["result_id"], name: "index_job_profiles_on_result_id"
   end
 
