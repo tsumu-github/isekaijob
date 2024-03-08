5f6197896cdb9154c55afc6d57185a3bff3217ca
tsumu-github <business000t2r@gmail.com>
2024-03-09 00:23:38 +0900

    25:webpack.config.jsに追記_4

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,15 @@
 const path = require('path');
 
-const { environment } = require('@rails/webpacker')
-
-const customConfig = {
-  resolve: {
-    fallback: {
-      dgram: false,
-      fs: false,
-      net: false,
-      tls: false,
-      child_process: false
-    }
-  }
+module.exports = {
+  mode: 'development',
+  entry: './app/javascript/application.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  node: {
+    global: false,
+    __filename: false,
+    __dirname: false,
+  },
 };
-
-environment.config.delete('node.dgram')
-environment.config.delete('node.fs')
-environment.config.delete('node.net')
-environment.config.delete('node.tls')
-environment.config.delete('node.child_process')
-
-environment.config.merge(customConfig);
-
-module.exports = environment
