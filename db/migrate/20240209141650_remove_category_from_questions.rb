4775690ea89affe7dddd707a6f38d00284bf55ba
tsumu-github <business000t2r@gmail.com>
2024-03-09 16:30:37 +0900

    33:カラム削除処理削除2

--- db/migrate/20240209141650_remove_category_from_questions.rb
+++ db/migrate/20240209141650_remove_category_from_questions.rb
@@ -1,5 +1,5 @@
 class RemoveCategoryFromQuestions < ActiveRecord::Migration[7.0]
   def change
-    remove_column :questions, :category, :string
+    #remove_column :questions, :category, :string
   end
 end
